1829c4bac95895774ca21f280b8548039b37c88f
ShahriarAlvi <alam.shahriaralvi@gmail.com>
2025-10-27 21:22:42 +0600

    echo(cors): cors implmeneted and controller refactored

--- backend/src/main/java/com/example/urlshortener/controller/RedirectController.java
+++ backend/src/main/java/com/example/urlshortener/controller/RedirectController.java
@@ -2,12 +2,14 @@ package com.example.urlshortener.controller;
 
 import com.example.urlshortener.service.UrlService;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class RedirectController {
     private final UrlService urlService;
